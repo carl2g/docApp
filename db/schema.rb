0e8094ff4fd9401c154f21171fe308e36e9ff7f6
Kw11 <alexandre.caila@epitech.eu>
2019-02-06 16:16:37 +0100

    merging

--- db/schema.rb
+++ db/schema.rb
@@ -38,11 +38,6 @@ ActiveRecord::Schema.define(version: 2019_02_06_140014) do
     t.index ["doctors_id"], name: "index_patients_on_doctors_id"
   end
 
-  create_table "user_status", force: :cascade do |t|
-    t.integer "status", default: 0
-    t.index [nil], name: "index_user_status_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
