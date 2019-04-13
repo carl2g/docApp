7b9355284301b0f814708ff881dbd6ce4886fd59
Kw11 <alexandre.caila@epitech.eu>
2019-04-13 14:38:54 +0200

    [ADD] unit test on models
    unittest
    
    update test for user model
    
    model testing

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid generate note" do
+    patient = Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    patient.addModule(1)
+    note = Note.generateNote(1, '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}', patient.user_id)
+    assert note.valid?, "note should be valid"
+  end
+
 end
