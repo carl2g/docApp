b7b482e9c66d77c5752eed1f83f405077aac97a3
Kw11 <alexandre.caila@epitech.eu>
2019-06-03 14:42:53 +0200

    quick save

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -1,10 +1,35 @@
 require 'test_helper'
 
 class PatientTest < ActiveSupport::TestCase
-  # test "valid patient" do
-  #   patient = Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-  #   assert Patient.take
-  # end
+
+  test "valid createPatient" do
+    assert Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+  end
+
+  test "generate patient with invalid params - no first_name" do
+    Patient.createPatient(last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    assert_not User.find_by(email: 'test@hotmail.com')
+  end
+
+  test "generate patient with invalid params - no last_name" do
+    Patient.createPatient(first_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    assert_not User.find_by(email: 'test@hotmail.com')
+  end
+
+  test "generate patient with invalid params - no email" do
+    Patient.createPatient(first_name: 'test', last_name: 'test', password: 'testest')
+    assert_not User.find_by(first_name: 'test', last_name: 'test')
+  end
+
+  test "generate patient with invalid params - no password" do
+    Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com')
+    assert_not User.find_by(email: 'test@hotmail.com')
+  end
+
+  test "generate patient with invalid params - short password" do
+    Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'test')
+    assert_not User.find_by(email: 'test@hotmail.com')
+  end
 
   # test "invalid patient" do
   #   patient = Patient.createPatient(first_name: 'test', last_name: 'test', email: 'test@hotmail.com')
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,78 +2,57 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "valid user without token" do
-    user = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    assert user.valid?
+  test "generate user with valid params" do
+    User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    # user = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    # user.save
+ #   u = User.find_by(email: 'test@hotmail.com')
+#    puts "#{u.inspect}"
+    assert User.find_by(email: 'test@hotmail.com')
   end
 
-  test "valid user with token" do
-    user = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest', login_token: 'tokentest')
-    assert user.valid?
+  test "generate user with invalid params - no first_name" do
+    User.generate_user(last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    assert_not User.find_by(email: 'test@hotmail.com')
   end
 
-  test "invalid user without first_name" do
-    user = User.new(last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    refute user.valid?, 'user is valid without first_name'
-    assert_not_nil user.errors[:first_name], 'no validation error without first_name'
+  test "generate user with invalid params - no last_name" do
+    User.generate_user(first_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    assert_not User.find_by(email: 'test@hotmail.com')
   end
 
-  test "invalid user without last_name" do
-    user = User.new(first_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    refute user.valid?, 'user is valid without last_name'
-    assert_not_nil user.errors[:last_name], 'no validation error without last_name'
+  test "generate user with invalid params - no email" do
+    User.generate_user(first_name: 'test', last_name: 'test', password: 'testest')
+    assert_not User.find_by(first_name: 'test', last_name: 'test')
   end
 
-  test "invalid user without email" do
-    user = User.new(first_name: 'test', last_name: 'test', password: 'testest')
-    refute user.valid?, 'user is valid without email'
-    assert_not_nil user.errors[:email], 'no validation error without email'
+  test "generate user with invalid params - no password" do
+    User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com')
+    assert_not User.find_by(email: 'test@hotmail.com')
   end
 
-  test "invalid user without password" do
-    user = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com')
-    refute user.valid?, 'user is valid without password'
-    assert_not_nil user.errors[:password], 'no validation error without password'
+  test "generate user with invalid params - short password" do
+    User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'test')
+    assert_not User.find_by(email: 'test@hotmail.com')
   end
 
-  test "invalid user with incorrect password" do
-    user = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'test')
-    refute user.valid?, 'user is valid with incorrect password'
-    assert_not_nil user.errors[:password], 'no validation error with incorrect password'
-  end
-
-  test "check user uniqueness" do
-    user1 = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    user2 = User.new(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-
-    user1.save
-    user2.save
-    assert_not (User.where(email: 'test@hotmail.com').size() > 1), "created 2 user with same email"
-  end
-
-  test "valid generate_user" do
-    user = User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    assert user.valid?
-  end
-
-  test "invalid generate_user" do
-    user = User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com')
-    refute user.valid?, 'user is invalid when using generate_user with invalid params'
-    assert_not_nil user.errors[:password], 'no validation error without password'
-  end
-
-  test "valid user authentification" do
+  test "valid authenticate" do
     User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
     assert User.authenticate('test@hotmail.com', 'testest')
   end
 
-  test "invalid user authentification, bad pasword" do
+  test "invalid authenticate - invalid password" do
     User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
-    refute User.authenticate('test@hotmail.com', 'testesta'), "could authentificate on user with incorrect password"
+    assert_nil User.authenticate('test@hotmail.com', 'testesta')
   end
 
-  test "invalid user authentification without user account" do
-    refute User.authenticate('test@hotmail.com', 'testest'), "could authentificate on user without account"
+  test "invalid authenticate - invalid email" do
+    User.generate_user(first_name: 'test', last_name: 'test', email: 'test@hotmail.com', password: 'testest')
+    assert_nil User.authenticate('test@hotmail.fr', 'testest')
+  end
+
+  test "invalid authenticate" do
+    assert_nil User.authenticate('test@hotmail.com', 'testest')
   end
 
 end
