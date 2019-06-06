19fe05b91545f8902dd57c2f18ea24078d56b234
kiwi <alexandre.caila@epitech.eu>
2019-06-06 18:46:07 +0200

    code style

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -9,7 +9,7 @@ class SessionControllerTest < ActionDispatch::IntegrationTest
     test "valid logout" do
         post "/api/login", params: {password: "12345678", email: "patient@gmail.com"}
         h = JSON.parse(@response.body)
-        post "/api/logout", headers: {'Authorization' => h["login_token"]} 
+        post "/api/logout", headers: {'Authorization' => h["login_token"]}
         assert_response :success
     end
 end
