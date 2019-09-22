00141d38ccc27d78f358deb862c731920d6618e4
alexandre <alexandre.caila@epitech.eu>
2019-09-22 05:40:38 +0200

    quick fix

--- app/controllers/api/doctors/patients_controller.rb
+++ app/controllers/api/doctors/patients_controller.rb
@@ -5,7 +5,6 @@ class Api::Doctors::PatientsController < ApplicationController
 		user_attrs = [:id, :email, :first_name, :last_name, :birthdate, :civility]
 		patients = current_doctor.patients.distinct
 		render json: patients.to_json({ except: [:id, :user_id],
-			:name => "lololol",
 			include: {
 				user: { only: user_attrs },
 				general_units: { only: [:id, :name] }
