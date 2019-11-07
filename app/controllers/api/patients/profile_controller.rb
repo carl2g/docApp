f33f9a2573861c84ab42f9877a4618b572944c3f
alexandre <alexandre.caila@epitech.eu>
2019-11-07 11:29:42 +0100

    [ADD] profile controler

--- app/controllers/api/patients/profile_controller.rb
+++ app/controllers/api/patients/profile_controller.rb
@@ -1,2 +1,10 @@
 class Api::Patients::ProfileController < ApplicationController
+
+  before_action :authenticate_user
+
+  def index
+    render json: current_patient.user.to_json({only: [:id, :email, :first_name, :last_name, :birthdate, :civility] }), status: :ok
+  end
+
+
 end
