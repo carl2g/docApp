e48db0a2da707f0a6f21e7d1c7519c008efd3174
alexandre <alexandre.caila@epitech.eu>
2020-01-03 15:15:22 +0100

    remove trailling spaces

--- app/controllers/api/admins/notes_controller.rb
+++ app/controllers/api/admins/notes_controller.rb
@@ -1,6 +1,6 @@
 class Api::Admins::NotesController < Api::Admins::AdminsController
 	before_action :authenticate_user, except: [:is_admin?]
-	
+
 	def update
 		note = Note.find_by(id: params[:id])
 		if note.update(permited_params)
@@ -19,7 +19,7 @@ class Api::Admins::NotesController < Api::Admins::AdminsController
 	end
 
 	private
-	
+
 		def permited_params
 			params.require(:note).permit([:unit_id, :data])
 	 	end
