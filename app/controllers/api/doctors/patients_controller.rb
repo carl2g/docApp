e7f79326ee0446bccebb756a078db5faf29f4287
alexandre <alexandre.caila@epitech.eu>
2020-01-05 11:39:52 +0100

    info patient for doctor

--- app/controllers/api/doctors/patients_controller.rb
+++ app/controllers/api/doctors/patients_controller.rb
@@ -12,4 +12,31 @@ class Api::Doctors::PatientsController < ApplicationController
 		}), status: :ok
 	end
 
+	def profile
+		patient = Patient.find_by(id: params[:id])
+		if patient
+			render json: patient.user.to_json(user_attr), status: :ok
+		else
+			render json: { errors: "Patient you tried to get profile informations doesn't exist: #{params[:id]}" }, status: :not_found
+		end
+	end
+
+private
+
+	def user_attr
+		[
+			:first_name,
+			:last_name,
+			:phone_number,
+			:email,
+			:civility,
+      :birthdate,
+      :picture
+		]
+	end
+
+	def permited_params
+		params.require(:id)
+	end
+
 end
