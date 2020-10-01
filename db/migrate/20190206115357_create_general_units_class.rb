72755ff7241ab70fc1cda27d676158d3dedae07e
carl2g <degentilecarl@gmail.com>
2020-10-01 10:03:17 +0200

    save work

--- db/migrate/20190206115357_create_general_units_class.rb
+++ db/migrate/20190206115357_create_general_units_class.rb
@@ -1,9 +1,10 @@
 class CreateGeneralUnitsClass < ActiveRecord::Migration[5.2]
-  	def change
+  	def up
 	  	create_table 		:general_units do |t|
 	  		t.string 		:name
 	  		t.string 		:icon
 	  		t.string 		:color
 	  	end
+		GeneralUnit.create!({name: 'diabetes',  color: 'blue',  icon: 'to be defined'})
   	end
 end
--- db/migrate/20190526130448_filter.rb
+++ db/migrate/20190526130448_filter.rb
@@ -1,7 +1,19 @@
 class Filter < ActiveRecord::Migration[5.2]
-  def change
-    add_column :general_units,  :filter,    :json, default: {}
-    add_column :units,          :filter,    :json, default: {}
-    add_column :notes,          :filter,    :json, default: {}
-  end
+	def up
+	    add_column :general_units,  :filter,    :json, default: {}
+	    add_column :units,          :filter,    :json, default: {}
+	    add_column :notes,          :filter,    :json, default: {}
+		diabet = GeneralUnit.find_by(name: "diabetes")
+		diabet.update(filter: {
+		    only: [
+		        :Glycemie,
+		        :Glucide,
+		        :InsulineAvRepas,
+		        :InsulineApRepas,
+		        :InsulineAJeun,
+		        :date,
+		        :heure
+		    ]
+		})
+	end
 end
